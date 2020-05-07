4cbd5331f8a24747e30ae685d114b9a3f5817638
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-05-07 12:42:11 +0200

    add now task in js

--- main.js
+++ main.js
@@ -9,3 +9,33 @@ document.querySelector('.clock span').textContent = `${hours}:${minutes}:${secon
 }
 
 setInterval(clock, 1000)
+
+
+
+const endTime = new Date('2021-01-25 23:00:00');
+
+const spanD = document.querySelector('span.d');
+
+const spanH = document.querySelector('span.h');
+
+const spanM = document.querySelector('span.m');
+
+const spanS = document.querySelector('span.s');
+
+
+setInterval(() => {
+    const nowTime = new Date().getTime();
+    const diffTime = Math.floor((endTime - nowTime)/1000);
+
+    const days = Math.floor((endTime / (1000 * 60 * 60 * 24)) - (nowTime / (1000 * 60 * 60 * 24)));
+    const hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) % 24);
+
+    const minutes = Math.floor(endTime / (1000 * 60) - nowTime / (1000 * 60) % 60)
+
+    const sec = Math.floor((endTime / 1000 - nowTime / 1000) % 60);
+
+    spanD.textContent = days;
+    spanH.textContent = hours;
+    spanM.textContent = minutes;
+    spanS.textContent = sec;
+}, 1000)

f03c8deee981580bc87de5b405d3b704a3f2777d
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-05-07 11:36:56 +0200

    fix bug in index and main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+const time = new Date();
+
+const seconds = time.getSeconds();
+const minutes = time.getMinutes();
+const hours = time.getHours();
+
+document.querySelector('.clock span').textContent = `${hours}:${minutes}:${seconds}`
